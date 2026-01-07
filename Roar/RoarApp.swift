2e6ca7956ffa42c4bd90a0716f6d6f8dbce824cd
Xavier Williams <51391036+SamuraiCali@users.noreply.github.com>
2026-01-07 10:13:13 -0500

    Initial Commit

---
+++ Roar/RoarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoarApp.swift
+//  Roar
+//
+//  Created by Xavier Williams on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct RoarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
